8afa7c6bf5b3bef576aee586a865580217614a3b
emre15900 <79264023+emre15900@users.noreply.github.com>
2022-04-15 03:29:43 +0300

    feed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import { ChatEngine } from "react-chat-engine";
-import ChatFeed from "./components/ChatFeed";
-import "./App.css";
+
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = "ce16b0db-ccbd-439d-a2ba-b346fe85335e";
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="ce16b0db-ccbd-439d-a2ba-b346fe85335e"
-      userName="emresayin"
-      userSecret="123456"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
+
+// infinite scroll, logout, more customizations...
 
 export default App;
